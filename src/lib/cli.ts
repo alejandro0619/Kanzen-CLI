5cb425aa8f1b3213edb4fb619a8580405064a336
spaghetti <alejandrolpz0619@gmail.com>
2021-07-23 14:06:17 -0400

    📍 CLI - selectABook method added.

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,26 +1,37 @@
 import inquirer from 'inquirer';
 import { Commands } from '../utils/Commands.js';
 import LibGen from './search.js'
+import IBooks  from '../interfaces/IBooks'
 export default class Books{
 
-  private async input():Promise<void> {
-    console.clear();
-    const answers = await inquirer.prompt({
+  private async input():Promise<IBooks[] | void> {
+    try {
+    const answers: {} = await inquirer.prompt({
       type: 'input',
       name: 'search',
       message: '🔎 Type the title of the book🔍'
     });
-    if (answers['search'] !== '') {
-      console.log(`searching for ${answers['search']}...\n Please wait.`);
+      if (answers['search'] !== '') {
+      console.log(`searching for ${answers['search']}...\nPlease wait.`);
       const Libgen = new LibGen();
-      const searchBook = await Libgen.searchBookByTitle(answers['search']);
-      console.log(searchBook);
-    } else {
-      console.log('endpoint')
+      console.log('worked!')
+      return await Libgen.searchBookByTitle(answers['search']);
+      } else {
+          return console.log('Please, enter a query to search.')
+        }
+    }
+    catch (err) {
+      throw new Error('An error has ocurred')
     }
   }
+  private async selectABook() {
+    const books: IBooks[] | void = await this.input();
+    
+    
+  }
+
   public async search() {
-    const answers = await inquirer.prompt({
+    const answers: {} = await inquirer.prompt({
       message: '📖 Search a book by 📖',
       type: 'list',
       name: 'command',
